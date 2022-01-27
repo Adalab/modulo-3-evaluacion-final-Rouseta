b667c212498b03bdabfae9e929a3709c7fb990da
Rosa Donoso <rosa.donoso.requena@outlook.de>
2022-01-27 13:31:34 +0100

    publish in GitHub Pages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import "../styles/components/Footer.scss";
 
 function Footer() {
-    return <footer className="footer"><a href="https://www.linkedin.com/in/rosa-donoso-requena/">&copy; Rouseta 2021</a></footer>;
+    return <footer className="footer"><a className="linkedin" href="https://www.linkedin.com/in/rosa-donoso-requena/ " target="_blank">&copy; Rouseta 2021</a></footer>;
 }
 export default Footer;
